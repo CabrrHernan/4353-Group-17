d48033f3430d3f8c0e388795c781c2da51f84547
Hernan Cabrera <H123cabrera@gmail.com>
2024-09-17 20:40:08 -0500

    Css for even and volunteer forms

--- volunteers/src/Forms pages/VolunteerMatchingForm.js
+++ volunteers/src/Forms pages/VolunteerMatchingForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './VolunteerMatchingForm.css';
 
 // Mock data for demonstration (replace with actual database calls)
 const volunteerData = [
